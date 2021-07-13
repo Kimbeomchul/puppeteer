07b1767e296005a3b77ae440728c1ad5792e6ade
BeomChul Kim <2017253083@yonsei.ac.kr>
2021-07-13 22:58:46 +0900

    주석 설명
    설명 추가

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# puppeteer
-puppeteer
+# web scraping with puppeteer
+[스크랩 사이트 주소](https://vibe.naver.com/chart/total)
+
+### scrap_vibe
+[scrap_with_axios](https://github.com/Kimbeomchul/node_scraping)
+
+### how to use
+```
+npm install
+npm start
+```
+
+### used
+- nodejs
+- puppeteer
+
+### !!!!
+- page.evaluate에서 function 변수를 넣으니까 에러가 발생.. 하단의 코드와 같이 해결할 수 있었다.
+```
+page.evaluate((a, b) => { // 여기위에만 작성했다가 에러뜨는걸 못찾았었다...
+   // 여기서 a,b변수를 사용할 수 있다.
+}, a, b);  
+```
+
+### Demo
+
--- index.js
+++ index.js
@@ -3,22 +3,24 @@ const puppeteer = require("puppeteer");
 
 (async ()=> {
 
-const browser = await puppeteer.launch({ headless: false });  // 실제 동작을 볼 수 있도록 headless는 false처리 
-const page = await browser.newPage(); 
+    const browser = await puppeteer.launch({ headless: false });  // 실제 동작을 볼 수 있도록 headless는 false처리 
+    const page = await browser.newPage(); 
 
 
-await page.goto('https://vibe.naver.com/chart/total' , {waitUntil :"networkidle2"}); // VIBE 순위페이지로 이동
+    await page.goto('https://vibe.naver.com/chart/total' , {waitUntil :"networkidle2"}); // VIBE 순위페이지로 이동
 
 
-let song_data =[]; // 정보담을 배열 
+    let song_data =[]; // 정보담을 배열 
 
-const data_len = await page.$$eval("#content > div.track_section > div:nth-child(1) > div > table > tbody > tr", (data) => data.length); 
-for (let i = 0; i < data_len; i++) { // 배열에 하나씩 넣기 
-    song_data.push(await scrap_song(i+1));
-}    
+    // tbody로 탑100이 들어있으므로 탑100 길이가져오기
+    const data_len = await page.$$eval("#content > div.track_section > div:nth-child(1) > div > table > tbody > tr", (data) => data.length); 
+    for (let i = 0; i < data_len; i++) { // 배열에 하나씩 넣기 
+        song_data.push(await scrap_song(i+1));
+    }    
 
+    //함수 반복접근으로 음원정보 가져오기
     async function scrap_song(index){
-        let data = await page.evaluate((index)=>{
+        let data = await page.evaluate((index)=>{ // page.evaluate에 변수넣었을때 인식을 못해서 애먹었다능.. 
              
             rank = document.querySelector('#content > div.track_section > div:nth-child(1) > div > table > tbody > tr:nth-child('+ index +') > td.rank > span').innerText;
             song = document.querySelector('#content > div.track_section > div:nth-child(1) > div > table > tbody > tr:nth-child('+ index +') > td.song > div.title_badge_wrap > span > a').innerText;
@@ -31,7 +33,7 @@ for (let i = 0; i < data_len; i++) { // 배열에 하나씩 넣기
                 artist,
                 album_img
             }
-        }, index);
+        }, index); // 여기다 index를 추가하여 해결가능
         return data;
     }
    
